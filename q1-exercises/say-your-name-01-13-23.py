5a60b3461116f25fa9d39c5b2692dea5c5e629b6
jerrvon <jerrvonewing.cs@gmail.com>
2023-01-13 10:58:42 -0600

    Formatting deciaml places

--- q1-exercises/say-your-name-01-13-23.py
+++ q1-exercises/say-your-name-01-13-23.py
@@ -1,1 +1,10 @@
+# Print your name
 print("Jerrvon")
+
+# Bank Robbery Money
+# I just robbed a bank with some of my friends.  We got away with $19,867,324,678,987.99, and now we have to split it up 5 ways.  Using the cash  variable I've already defined for you, print out the dollar amount each robber gets to keep.  (divide cash  by 5 and print the answer out)
+# p.s. I haven't actually robbed a bank.  Yet.
+cash:float = 19867324678987.99
+split = round(cash/5,2)                         # Round to two decimal places
+formattedSplit = "{:.2f}".format(split)         # Format so that the 2nd decimal place is persisted
+print(formattedSplit)
