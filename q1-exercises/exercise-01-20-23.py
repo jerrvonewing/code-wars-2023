fe6d94ad64ba9025a0ed7c6b3c58ee897ddd88ee
jerrvon <jerrvonewing.cs@gmail.com>
2023-01-19 13:14:25 -0600

    Added Exercise 3: Make some variables

---
+++ q1-exercises/exercise-01-20-23.py
@@ -0,0 +1,24 @@
+# Exercise 3: Lesson 49 
+# Make Some Variables!
+########################################################################
+# Define a variable named city and set it equal to any string
+city = "Chicago"
+
+# Define a variable named price and set it equal to any float
+price = 15.23
+
+# Define a variable named high_score and set it equal to any int
+high_score = 213
+
+# Define a variable named is_having_fun and set it to a boolean value
+is_having_fun = True
+
+# Exercise 4: Lesson 51
+# String Escape Sequences
+########################################################################
+#
+
+
+# Exercise 5: Lesson 53
+# String Concatenation Exercise
+########################################################################
