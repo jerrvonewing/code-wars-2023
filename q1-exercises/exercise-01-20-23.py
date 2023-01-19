3a4973631fa40a1dfcb06ea557cbb5dc041783af
jerrvon <jerrvonewing.cs@gmail.com>
2023-01-19 13:57:41 -0600

    Added Exercise 5: Concatenation Exercise

--- q1-exercises/exercise-01-20-23.py
+++ q1-exercises/exercise-01-20-23.py
@@ -37,6 +37,7 @@ print(quotation)
 # Set the variable called greeting  to some greeting, e.g. "hello".
 # Set the variable called name  to some name, e.g. "Heisenberg".
 # Then set the variable called greet_name  so that it concatenates greeting , name , and a space " " between them.
-greeting = None
-name = None
-greet_name = None
+greeting = "Hello"
+name = "Heisenberg"
+greet_name = greeting + " " + name
+print(greet_name)
