a888aa43c22697df22abd1bff5b29030c1e9b2c3
jerrvon <jerrvonewing.cs@gmail.com>
2023-01-19 13:45:45 -0600

    01-20-23: Print variables in exercise 3

--- q1-exercises/exercise-01-20-23.py
+++ q1-exercises/exercise-01-20-23.py
@@ -13,12 +13,31 @@ high_score = 213
 # Define a variable named is_having_fun and set it to a boolean value
 is_having_fun = True
 
+# Print the variables
+print("The city is", city, " and the price is", price, ". You need at least", high_score, "points to get to the high score score.")
+
 # Exercise 4: Lesson 51
 # String Escape Sequences
 ########################################################################
-#
+# Set the message variable equal to any string containing a new-line escape sequence
+message = ""
 
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = ""
+
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = ""
 
 # Exercise 5: Lesson 53
 # String Concatenation Exercise
 ########################################################################
+# Set the variable called greeting  to some greeting, e.g. "hello".
+# Set the variable called name  to some name, e.g. "Heisenberg".
+# Then set the variable called greet_name  so that it concatenates greeting , name , and a space " " between them.
+greeting = None
+name = None
+greet_name = None
