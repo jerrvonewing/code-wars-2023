c613910cd4e68eb701708a74d97648ffd1be4584
jerrvon <jerrvonewing.cs@gmail.com>
2023-01-27 11:05:58 -0600

    Added comments

--- q1-exercises/exercise-01-27-23.py
+++ q1-exercises/exercise-01-27-23.py
@@ -1,25 +1,30 @@
+# Exercise 6: Lesson 56 - Formatted Strings
+
 # Set the variable called first to your first name.
-
 # Set the variable called last to your last name.
-
 # Then set the variable called formatted that interpolates both
 # using the .format() method. Make sure you follow this pattern:
 
 first = 'Walter'
 last = 'White'
-formatted = "First Name: {first}, Last Name: {last}".format(first=first, last=last)
+formatted = "First Name: {}, Last Name: {}".format(first, last)
 print(formatted)
 
 # Check if choice is 7
 from random import randint
 choice = randint(1,10)
 
+
+
+# Exercise 7: Lesson 63 - Lucky Number
 if choice == 7:
    print("Lucky you!") 
 else:
     print("Not so lucky!")
 print(choice)
 
+
+# Exercise 9: Lesson 69 - Food Classifying
 # Assigns a random value as the food choice
 from random import choice
 food = choice(['apple','grape','bacon','steak','worm','dirt'])
@@ -32,6 +37,10 @@ if (food == 'bacon' or food == 'steak'):
 else:
     print("yuck!")
 
+
+
+
+# Exercise 10: Lesson 73 - Positive or Negative?
 # Checks if value is positive or negative
 
 from random import randint
